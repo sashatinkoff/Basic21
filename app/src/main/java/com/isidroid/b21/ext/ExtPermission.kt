564b36e9af750995dde76e2ea48613c98a907500
Aleksandr Tamakov <sashatinkoff@gmail.com>
2020-07-16 11:01:48 +0500

    Permissions

---
+++ app/src/main/java/com/isidroid/b21/ext/ExtPermission.kt
@@ -0,0 +1,43 @@
+package com.isidroid.b21.ext
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+
+fun AppCompatActivity.permission(
+    permission: String,
+    onGranted: () -> Unit,
+    onShowRationale: (() -> Unit)? = null,
+    onDenied: (() -> Unit)? = null
+) {
+    val code = ContextCompat.checkSelfPermission(this, permission)
+    when {
+        code == PackageManager.PERMISSION_GRANTED -> onGranted()
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && shouldShowRequestPermissionRationale(permission) -> onShowRationale?.invoke()
+        else -> registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) onGranted() else onDenied?.invoke()
+        }.launch(permission)
+    }
+}
+
+fun Fragment.permission(
+    permission: String,
+    onGranted: () -> Unit,
+    onShowRationale: (() -> Unit)? = null,
+    onDenied: (() -> Unit)? = null
+) {
+    val code = ContextCompat.checkSelfPermission(requireActivity(), permission)
+    when {
+        code == PackageManager.PERMISSION_GRANTED -> onGranted()
+        Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && shouldShowRequestPermissionRationale(permission) -> onShowRationale?.invoke()
+        else -> registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (it) onGranted() else onDenied?.invoke()
+        }.launch(permission)
+    }
+}
