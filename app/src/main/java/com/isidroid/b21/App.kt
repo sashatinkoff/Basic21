a5ad5fdffdcc6120d05937ef86c99ab6d0e778e0
Aleksandr Tamakov <sashatinkoff@gmail.com>
2020-07-17 11:05:09 +0500

    Add Realm

--- app/src/main/java/com/isidroid/b21/App.kt
+++ app/src/main/java/com/isidroid/b21/App.kt
@@ -1,17 +1,30 @@
 package com.isidroid.b21
 
 import android.app.Application
-import android.content.Context
 import com.isidroid.b21.di.DaggerAppComponent
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import io.realm.RealmMigration
 import timber.log.Timber
 
 class App : Application() {
     internal val appComponent by lazy { DaggerAppComponent.factory().create(this) }
 
-
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        configureRealm()
+    }
+
+    private fun configureRealm() {
+        Realm.init(this)
+        val migration: RealmMigration? = null
+        val builder = RealmConfiguration.Builder()
+            .name("database.realm")
+            .schemaVersion(1L)
+
+        migration?.let { builder.migration(it) } ?: builder.deleteRealmIfMigrationNeeded()
+        builder.build().apply { Realm.setDefaultConfiguration(this) }
     }
 }
 
---
+++ app/src/main/java/com/isidroid/b21/realm/DataModel.kt
@@ -0,0 +1,63 @@
+package com.isidroid.b21.realm
+
+import androidx.annotation.CallSuper
+import io.realm.RealmModel
+import io.realm.kotlin.deleteFromRealm
+import io.realm.kotlin.isManaged
+
+interface DataModel : RealmModel {
+    @CallSuper
+    fun save() = apply {
+        if (onSave())
+            YRealm.executeOnMainThread { it.insertOrUpdate(this) }
+    }
+
+
+    @CallSuper
+    fun delete() = apply {
+        YRealm.executeOnMainThread {
+            if (onDelete()) {
+                var item2 = this
+                if (!isManaged()) item2 = it.copyToRealmOrUpdate(item2)
+                item2.deleteFromRealm()
+            }
+        }
+    }
+
+    @CallSuper
+    fun update() = apply {
+        val json = YRealm.gson.toJson(arrayListOf((this)))
+        YRealm.executeOnMainThread { it.createOrUpdateAllFromJson(javaClass, json) }
+    }
+
+    @CallSuper
+    fun refresh() = apply { YRealm.refresh() }
+
+    /**
+     * Executes before saving the object
+     * @return if true then the object will be saved
+     */
+    fun onSave(): Boolean {
+        return true
+    }
+
+    /**
+     * Executes before deleting the object
+     * @return if true then the object will be deleted
+     */
+    fun onDelete(): Boolean {
+        return true
+    }
+
+    /**
+     * Executes before updating the object
+     * if some fields are null (or empty) in the UPDATING object they won't be updated in the object
+     * that is already is database
+     * @return if true then the object will be updated
+     */
+    fun onUpdate(): Boolean {
+        return true
+    }
+}
+
+
