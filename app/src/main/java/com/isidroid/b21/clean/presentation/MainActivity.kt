05f1522e36f34bee4bef8a75e3f113ee726f17c0
A. Tamakov <sashatinkoff@gmail.com>
2021-03-04 15:09:44 +0500

    clean code

--- app/src/main/java/com/isidroid/b21/clean/presentation/MainActivity.kt
+++ app/src/main/java/com/isidroid/b21/clean/presentation/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
 import com.isidroid.b21.R
 import com.isidroid.b21.appComponent
+import com.isidroid.b21.models.db.Post
 import com.isidroid.b21.utils.core.BindActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : BindActivity(layoutRes = R.layout.activity_main) {
     private val viewModel by viewModels<MainViewModel> { viewModelFactory }
@@ -16,6 +18,5 @@ class MainActivity : BindActivity(layoutRes = R.layout.activity_main) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
 
-        button.setOnClickListener { viewModel.recreate() }
     }
 }
--- app/src/main/java/com/isidroid/b21/models/db/Post.kt
+++ app/src/main/java/com/isidroid/b21/models/db/Post.kt
@@ -2,6 +2,8 @@ package com.isidroid.b21.models.db
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Parcelize
-data class Post(val id: Int, val name: String): Parcelable
+data class Post(val id: Int, val name: String) : Parcelable {
+}
